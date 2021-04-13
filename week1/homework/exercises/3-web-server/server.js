8e113d554f83795c04723248fc30dbf9ffae4fbd
Mahmood <pejvakco@gmail.com>
2021-04-13 22:49:35 +0200

    finished project for homework week1

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -7,8 +7,49 @@ var http = require('http');
 //create a server
 let server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  const fs = require('fs');
+  let returnValue = '';
+
+  switch (req.url) {
+    case '/':
+      fs.readFile('./index.html', 'utf8', (error, data) => {
+        if (error) {
+          errorHandling(error);
+        } else {
+          res.writeHeader(200, { 'Content-Type': 'text/html' });
+          returnValue = data;
+        }
+      });
+      break;
+    case '/index.js':
+      fs.readFile('./index.js', 'utf8', (error, data) => {
+        if (error) {
+          errorHandling(error);
+        } else {
+          res.writeHeader(200, { 'Content-Type': 'text/js' });
+          returnValue = data;
+        }
+      });
+      break;
+    case '/style.css':
+      fs.readFile('./style.css', 'utf8', (error, data) => {
+        if (error) {
+          errorHandling(error);
+        } else {
+          res.writeHeader(200, { 'Content-Type': 'text/css' });
+          returnValue = data;
+        }
+      });
+      break;
+  }
+
+  res.write(returnValue); // Sends a response back to the client
+  res.end(); // Ends the response
 });
+
+const errorHandling = (error) => {
+  console.log(error);
+  throw new Error(error);
+};
 
 server.listen(3000); // The server starts to listen on port 3000
