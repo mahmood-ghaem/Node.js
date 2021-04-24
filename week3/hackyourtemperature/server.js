71d30d5667c762a7ce3cf4cb24c87f315d464bc3
Mahmood <pejvakco@gmail.com>
2021-04-24 23:55:53 +0200

    finished exc 1 and 2

--- week3/hackyourtemperature/server.js
+++ week3/hackyourtemperature/server.js
@@ -11,29 +11,37 @@ app.set('view engine', 'handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: false }));
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
 app.post('/weather', async (req, res) => {
-  const cityName = req.body.cityName;
-  if (cityName.length === 0) {
-    res.status(404).render('index', { result: 'City not found!' });
-    return;
-  } else {
-    res.status(200).render('index', {
-      weatherText: `The temperature in ${cityName} is ${await getTemperature(
-        cityName,
-      )} °C!`,
-    });
-  }
+    const cityName = req.body.cityName;
+    const temperature = await getTemperature(cityName);
+    if (cityName.length === 0) {
+        res.status(404).render('index', { result: 'Please enter a city name.' });
+        return;
+    }
+    else if (temperature === undefined) {
+        res.status(404).render('index', { result: 'Can not fetch data!' });
+        return;
+    }
+    else {
+        res.status(200).render('index', {
+            weatherText: `The temperature in ${cityName} is ${temperature} °C!`,
+        });
+    }
 });
 
 const getTemperature = async (cityName) => {
-  const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${API_KEY}`,
-  );
-  const temperature = response.data.main.temp.toFixed(0);
-  return temperature;
+    let returnValue;
+    try {
+        const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${API_KEY}`,
+        );
+        returnValue = response.data.main.temp.toFixed(0);
+    } catch (error) { }
+
+    return returnValue;
 };
 
 app.listen(3000);
